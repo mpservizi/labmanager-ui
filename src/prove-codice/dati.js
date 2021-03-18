1c350190d277ff5362e75a4d047d0a31191dc8c0
WorkPc <Malkit@Sandhu.ite>
2021-03-18 16:10:06 +0100

    Creato file html per test gantt

---
+++ src/prove-codice/dati.js
@@ -0,0 +1,81 @@
+const dati = {
+    data: [
+        {
+            id: 13,
+            text: 'L180 - 19.1',
+            start_date: '03-04-2018 00:00',
+            type: 'project',
+            render: 'split',
+            parent: 0,
+            progress: 0.5,
+            open: false,
+            duration: 11
+        },
+        {
+            id: 17,
+            text: 'Stage #1',
+            start_date: '03-04-2018 00:00',
+            duration: 1,
+            parent: '13',
+            progress: 0,
+            open: true
+        },
+        {
+            id: 18,
+            text: 'Stage #2',
+            start_date: '05-04-2018 00:00',
+            duration: 2,
+            parent: '13',
+            progress: 0,
+            open: true
+        },
+        {
+            id: 19,
+            text: 'Stage #3',
+            start_date: '08-04-2018 00:00',
+            duration: 1,
+            parent: '13',
+            progress: 0,
+            open: true
+        },
+        {
+            id: 20,
+            text: 'Stage #4',
+            start_date: '10-04-2018 00:00',
+            duration: 4,
+            parent: '13',
+            progress: 0,
+            open: true
+        },
+        {
+            id: 15,
+            text: 'L180 - 19.3',
+            type: 'project',
+            render: 'split',
+            parent: 0,
+            progress: 0,
+            open: false,
+            start_date: '03-04-2018 00:00',
+            duration: 11
+        },
+        {
+            id: 21,
+            text: 'Stage #1',
+            start_date: '03-04-2018 00:00',
+            duration: 4,
+            parent: '15',
+            progress: 0,
+            open: true
+        },
+        {
+            id: 22,
+            text: 'Stage #2',
+            start_date: '08-04-2018 00:00',
+            duration: 3,
+            parent: '15',
+            progress: 0,
+            open: true
+        }
+    ],
+    links: []
+};
