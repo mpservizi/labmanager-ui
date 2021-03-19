8cb0ec23e606e92b050727c6eaa03fb1f9063e1e
WorkPc <Malkit@Sandhu.ite>
2021-03-19 11:57:51 +0100

    Implementato parsing task dal server

--- src/modules/gantt/components/Gantt.vue
+++ src/modules/gantt/components/Gantt.vue
@@ -1,5 +1,10 @@
 <template>
-    <div ref="gantt" class="ganttBox"></div>
+    <div>
+        <div>
+            <v-btn @click="raggruppa">Raggruppa</v-btn>
+        </div>
+        <div ref="gantt" class="ganttBox"></div>
+    </div>
 </template>
  
 <script>
@@ -14,13 +19,63 @@ export default {
             }
         }
     },
-
+    data() {
+        return {
+            gruppoAttivo: false
+        };
+    },
     mounted: function () {
-        gantt.config.xml_date = '%Y-%m-%d';
+        gantt.plugins({
+            grouping: true
+        });
+
+        // gantt.config.task_height = 16;
+        // gantt.config.row_height = 40;
+
+        gantt.serverList('carichi', [
+            { key: 1, label: '19.1' },
+            { key: 2, label: '19.2' },
+            { key: 3, label: '19.3' }
+        ]);
+
+        gantt.templates.grid_row_class = gantt.templates.task_row_class = function (
+            start,
+            end,
+            task
+        ) {
+            if (task.$virtual) return 'summary-row';
+        };
+        gantt.templates.task_class = function (start, end, task) {
+            if (task.$virtual) {
+                // task.render = 'split';
+                // task.open = false;
+                return 'summary-bar';
+            }
+        };
+
+        // gantt.config.xml_date = '%Y-%m-%d';
+        gantt.config.xml_date = '%d-%m-%Y';
 
         gantt.init(this.$refs.gantt);
     },
-    methods: {},
+    methods: {
+        raggruppa() {
+            if (this.gruppoAttivo) {
+                gantt.groupBy(false);
+            } else {
+                gantt.groupBy({
+                    groups: gantt.serverList('carichi'),
+                    relation_property: 'idCarico',
+                    group_id: 'key',
+                    group_text: 'label'
+                });
+                // gantt.sort('start_date');
+            }
+            this.gruppoAttivo = !this.gruppoAttivo;
+
+            gantt.render();
+        }
+    },
     computed: {
         // Estaggo i dati dal store
         dati() {
@@ -39,7 +94,49 @@ export default {
 <style>
 @import '../libs/gantt/dhtmlxgantt.css';
 .ganttBox {
-    position: relative;
-    height: 100%;
+    /* position: relative; */
+    height: 800px;
+}
+
+.summary-row,
+.summary-row.odd {
+    background-color: #eeeeee;
+    font-weight: bold;
+}
+
+.gantt_grid div,
+.gantt_data_area div {
+    font-size: 12px;
+}
+
+.summary-bar {
+    opacity: 0;
+}
+
+.baseline {
+    position: absolute;
+    border-radius: 2px;
+    opacity: 0.6;
+    margin-top: -7px;
+    height: 12px;
+    background: #ffd180;
+    border: 1px solid rgb(255, 153, 0);
+}
+/* move task lines upper */
+.gantt_task_line,
+.gantt_line_wrapper {
+    margin-top: -9px;
+}
+
+.gantt_side_content {
+    margin-bottom: 7px;
+}
+
+.gantt_task_link .gantt_link_arrow {
+    margin-top: -12px;
+}
+
+.gantt_side_content.gantt_right {
+    bottom: 0;
 }
 </style>
