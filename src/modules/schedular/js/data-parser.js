e6fd3e551d37fca5c879acd7715f1ca2d3171f5f
mpservizi <info.mpservizi@gmail.com>
2021-03-20 18:39:20 +0000

    Finalizzato la funzione per calcolare il carico di lavoro delle risorse

--- src/modules/schedular/js/data-parser.js
+++ src/modules/schedular/js/data-parser.js
@@ -128,14 +128,14 @@ export async function parseRisorse(datiServer) {
  * @param {*} eventi 
  */
 export function calcolaCaricoRisorse(myScheduler, eventi) {
-    let filtri = eventi.filter(item => item.idRisorsa == 1);
+    let filtri = eventi.filter(item => item.idRisorsa < 50);
     let matrice = [];
     filtri.forEach(item => {
         let caricoTask = ricavaCaricoRisorsaTask(myScheduler, item);
         matrice.push(caricoTask);
     });
     let caricoRisorse = sommaDatiRisorsa(matrice);
-    // console.log(JSON.stringify(caricoRisorse));
+    console.log(JSON.stringify(caricoRisorse));
     // console.log(caricoRisorse);    
     return caricoRisorse;
 }
@@ -147,27 +147,36 @@ export function calcolaCaricoRisorse(myScheduler, eventi) {
 function sommaDatiRisorsa(matriceLoads) {
     let result = {}
     // console.log(matriceLoads);    
-                let richiesteAnalizzate = {};
+    let richiesteAnalizzate = {};
     matriceLoads.forEach(item => {
         //Record per la risorsa nel ogetto globale
         let objRisorsa = result[item.idRisorsa];
+        //Inizializzo la chiave id risorsa nel oggetto globale
+        if (!richiesteAnalizzate[item.idRisorsa]) {
+            richiesteAnalizzate[item.idRisorsa] = {}
+        }
         //Se l'ogetto globale non contiene i dati per questa risorsa
         if (!objRisorsa) {
             result[item.idRisorsa] = item.workLoad;
         } else {
+            //Ricavo id dei carichi presenti nel array
             let idCarichiItem = Object.keys(item.workLoad);
+            //Per ogni carico
             idCarichiItem.forEach(keyCarico => {
+                //Estraggo il workload del carico dal item della matrice
                 let caricoItem = item.workLoad[keyCarico];
+                //Verifico se lo stesso tipo di carico esiste nel oggetto globale
                 let caricoGlobale = objRisorsa[keyCarico];
-                //Se esite già un carico uguale nel oggetto globale
+                //Se esite già, vado ad integrare i dati
                 if (caricoGlobale) {
-                    sommaWorloadTaskInMatrice(caricoGlobale, caricoItem,richiesteAnalizzate);
-                }else{
-                    //aggiungo carico locale nel oggetto globale
+                    sommaWorloadTaskInMatrice(caricoGlobale, caricoItem, richiesteAnalizzate, item.idRisorsa);
+                } else {
+                    //Se il carico non è presente nel oggetto globale
+                    //Copio il carico
                     objRisorsa[keyCarico] = caricoItem;
                 }
             })
-        }   
+        }
     })
 
     return result;
@@ -179,12 +188,39 @@ function sommaDatiRisorsa(matriceLoads) {
  * @param {*} objLocale : Oggetto con il workload del tak
  * @param {*} richiesteAnalizzate : varibile che tiene traccia delle richieste già analizzate
  */
-function sommaWorloadTaskInMatrice(objGlobale, objLocale,richiesteAnalizzate) {
-    Object.keys(objGlobale).forEach(keyWeek => {
-        //Array workload carico, valori sono {days:n,idRequest:n}
+function sommaWorloadTaskInMatrice(objGlobale, objLocale, richiesteAnalizzate, idRisorsa) {
+    /**
+     * Bug:
+     * ObjGlobale e objLocale contengono lo stesso numero di valori ma con numerro di settimana diversa
+     * Es:
+     * globale contiene i dati della WK10, aggiunti dai task precedenti
+     * locale contiene i dati della WK11, dati del task.
+     * La ricerca viene fatta per ogni chiave del oggetto globale [WK10]
+     * In questo caso il carico locale è undefined, perchè serve key WK11
+     */
+
+    //Creo un array con tutti i dati con tutt le chavi del oggetto locale e globale
+    let tmpObj = { ...objGlobale, ...objLocale }
+    let chiaviWeek = Object.keys(tmpObj);
+
+    //Gli oggeti del workload hanno come chiave il numero della settiamana WK??
+    chiaviWeek.forEach(keyWeek => {
+        //Estraggo il workload in base alla settimana, valori sono {days:n,idRequest:n}
         let caricoGlobale = objGlobale[keyWeek];
         let caricoLocale = objLocale[keyWeek];
+
+        if (caricoGlobale != undefined && caricoLocale == undefined) {
+            // console.log('Wk globale diversa da week task');
+            return;
+        }
+        //la settimana del Carico del task non è presente nel carico globale
+        if (caricoGlobale == undefined && caricoLocale != undefined) {
+            objGlobale[keyWeek] = caricoLocale;
+            return;
+        }
+
         //Loop per ogni valore in 2 array
+        //Se entrambi i carichi hanno la stessa settimana
         if (caricoGlobale != undefined && caricoLocale != undefined) {
             caricoGlobale.forEach(objGlobale => {
                 caricoLocale.forEach(objLocale => {
@@ -192,11 +228,11 @@ function sommaWorloadTaskInMatrice(objGlobale, objLocale,richiesteAnalizzate) {
                     if (objGlobale.idRequest == objLocale.idRequest) {
                         //Sommo i giorni del carico locale in carico globale
                         objGlobale.days += objLocale.days;
-                    }else{
+                    } else {
                         // Stesso carico ma per una richiesta diversa
                         //Per evitare doppioni, Controllo se ho già analizzato questa test request
-                        if(richiesteAnalizzate[objLocale.idRequest]==undefined){
-                            richiesteAnalizzate[objLocale.idRequest]=true;
+                        if (richiesteAnalizzate[idRisorsa][objLocale.idRequest] == undefined) {
+                            richiesteAnalizzate[idRisorsa][objLocale.idRequest] = true;
                             caricoGlobale.push(objLocale);
                         }
                     }
@@ -232,7 +268,7 @@ function addValoreInKey(obj, key, valore) {
  * Risultato è un oggetto che per chiave ha id della risorsa
  * come valore un oggetto che ha come chiave il numero della settimana e come valore il giorno dei giorni occupati dal task
  */
-function ricavaCaricoRisorsaTask(myScheduler,item) {
+function ricavaCaricoRisorsaTask(myScheduler, item) {
     let caricoRisorsa = {};
     let arrWorkload = creaArrayCaricoTask(item);
     caricoRisorsa.idRisorsa = item.idRisorsa;
