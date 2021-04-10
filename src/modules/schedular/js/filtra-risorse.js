d35c2a8b1a85fcc8a5989b57e251ba23920a657c
HomePc <Lenovo@gmail.com>
2021-04-10 17:30:44 +0200

    Creato funzione per filtrare in base al carco

--- src/modules/schedular/js/filtra-risorse.js
+++ src/modules/schedular/js/filtra-risorse.js
@@ -1,22 +1,78 @@
 import { MyPlanner } from 'Moduli/schedular/js/my-planner.js';
+import { getListaRisorse } from '@/shared/liste/risorse-ciclatura.js';
 
-export function filtraByCarico(idCarico){
-    console.log('Filtrare i carichi : ' + idCarico);
 
+
+/**
+ * Filtra gli stalli di ciclatura compatibili con il tipo di carico indicato 
+ * @param {Number} idCarico : 1 = 19.1 ; 2= 19.2 ; 3 = 19.3
+ */
+
+export function filtraByCarico(idCarico) {
+    //Se carico non valido mostro tutte le risors 
+    if (!idCarico) {
+        filtraByNome();
+        return;
+    }
+    /**
+     * 19.1
+     * L180 - 8,9
+     * L232 - 6,7,8,9
+     * L2020 - 1,2
+     */
+    const STALLI_19_1 = [
+        3, 4,
+        5, 6, 7, 8,
+        9, 10
+    ];
+
+    /**
+     * 19.2
+     * L180 - 6,7
+     * L232 - 6,7,8,9
+     */
+    const STALLI_19_2 = [
+        1, 2,
+        5, 6, 7, 8,
+    ];
+
+    /**
+     * 19.3
+     * L180 - 6,7
+     * L232 - 6,7,8,9
+     * L2020 - 3,4,5,6
+     */
+    const STALLI_19_3 = [
+        1, 2,
+        5, 6, 7, 8,
+        11, 12, 13, 14
+    ];
+
+    const ElencoListe = [STALLI_19_1, STALLI_19_2, STALLI_19_3];
+    //In base al id del carico seleziono la lista id stalli.
+    //tolgo 1 dal id per trasformarlo in indice della matrice
+    let listaIds = ElencoListe[idCarico - 1];
+    let itemsFiltrati;
+    let risorse = MyPlanner.getListaRisorse();
+    itemsFiltrati = risorse.filter(function (item) {
+        return listaIds.includes(item.key);
+    });
+
+    MyPlanner.setListaFiltrata(itemsFiltrati);
 }
 /** Filtra le risorse
  * Riceve il parametro dal componente filtro
  */
- export function filtraByNome(valore) {
+export function filtraByNome(valore) {
     let risorse = MyPlanner.getListaRisorse();
     let itemsFiltrati;
     if (!valore || valore == 'all') {
         itemsFiltrati = risorse;
     } else {
-        itemsFiltrati = risorse.filter(function(item) {
+        itemsFiltrati = risorse.filter(function (item) {
             return item.label.includes(valore);
         });
     }
 
-    MyPlanner.setListaFiltrata(itemsFiltrati);    
+    MyPlanner.setListaFiltrata(itemsFiltrati);
 }
